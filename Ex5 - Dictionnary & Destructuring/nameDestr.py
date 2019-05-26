e854a136260cb5b7060e5ff4f794b02dd2b3ebee
PuffBallPoyo <46196874+PuffBallPoyo@users.noreply.github.com>
2019-05-26 19:21:50 +0200

    Update nameDestr.py

--- Ex5 - Dictionnary & Destructuring/nameDestr.py
+++ Ex5 - Dictionnary & Destructuring/nameDestr.py
@@ -2,8 +2,8 @@
 #le premier et le dernier nom en utilisant la destructuration
 
 noms = input("Entrez vos prénoms et votre nom : ")
-liste_noms = noms.split(" ")
+liste_noms = noms.split(" ") #Sépare un string et met chaque élément dans une liste
 
-prénom,*_,nom = liste_noms
+prénom,*_,nom = liste_noms #Affecte le premier nom et le dernier nom de la liste et affecte le reste à une variable facultative
 
 print("Bienvenue",prénom,nom)
