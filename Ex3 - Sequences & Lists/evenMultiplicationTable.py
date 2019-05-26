876ba02b4eea0914a163cea9147dc13d74586d9a
PuffBallPoyo <46196874+PuffBallPoyo@users.noreply.github.com>
2019-05-26 18:29:37 +0200

    Update evenMultiplicationTable.py

--- Ex3 - Sequences & Lists/evenMultiplicationTable.py
+++ Ex3 - Sequences & Lists/evenMultiplicationTable.py
@@ -1,10 +1,13 @@
+#Script qui permet d'afficher les tables de multiplication des
+#nombres pairs se trouvant entre deux nombres entrés par l'utilisateur
+
 nb1 = int(input("Tapez deux nombres pour en afficher les table de multiplication des nombres pairs:"))
 nb2 = int(input())
 
-evenRange = range(nb1,nb2,2) #Finds even numbers between nb1 and nb2
+evenRange = range(nb1,nb2,2) #Cherche des numéros pairs entre 'nb1' et 'nb2
 
 
-for i in evenRange[1:len(evenRange)] :
-    table = range(i, (i*10)+1, i) #Makes a multiplication table of an element in evenRange
+for i in evenRange[1:len(evenRange)] : #Ignore le premier et le dernier élément
+    table = range(i, (i*10)+1, i) #Genère une table de multiplication et l'affecte à la liste 'table'
     for j in table :
-        print(int(j/(table.index(j)+1)) ,'*' ,table.index(j)+1, '=', j,) #Prints the multiplication table
+        print(int(j/(table.index(j)+1)) ,'*' ,table.index(j)+1, '=', j,) #Affiche la table de multiplication
