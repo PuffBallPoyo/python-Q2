aca7c0fe5c062b9a61d67bb8cfaa58cfe86ef5e2
PuffBallPoyo <46196874+PuffBallPoyo@users.noreply.github.com>
2019-05-26 16:56:41 +0200

    condtion block example

---
+++ Ex1-2 - Structures/understanding_condition_blocks.1.py
@@ -0,0 +1,10 @@
+prix_repas = float(input("Prix du repas : "))
+nb_repas = int(input("Nombre de repas : "))
+prix_total = nb_repas * prix_repas
+
+#bloc de condition
+if nb_repas >= 10 : 
+    print("Reduction pour 10 repas ! ")
+prix_total *= 0.9
+print("À payer : ",prix_total,"euros")
+#Pas de réduction ici car le code se trouve en dehors du bloc
--- Ex1-2 - Structures/understanding_condition_blocks.2.py
+++ Ex1-2 - Structures/understanding_condition_blocks.2.py
@@ -1,7 +1,10 @@
 prix_repas = float(input("Prix du repas : "))
 nb_repas = int(input("Nombre de repas : "))
 prix_total = nb_repas * prix_repas
+
+#bloc de condition
 if nb_repas >= 10 : 
     print("Reduction pour 10 repas ! ")
     prix_total *= 0.9
     print("À payer : ",prix_total,"euros")
+#Réduction de 10%!
